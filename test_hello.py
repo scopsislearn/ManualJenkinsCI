352844d15d44b8a4ef97366350fdd79fda56fbf4
scopsislearn <150110177+scopsislearn@users.noreply.github.com>
2023-12-17 12:50:27 +0200

    Update test_hello.py

--- test_hello.py
+++ test_hello.py
@@ -5,5 +5,5 @@ class TestHello(unittest.TestCase):
   def test_hello(self):
     self.assertEqual(hello(), "Hello, Jenkins!")
 
-if __name__ == '__main__'
+if __name__ == '__main__':
   unittest.main()
